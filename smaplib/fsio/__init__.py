87219bb65ac195f191dffe3323beb0410f83c3b2
Benjamin RAIBAUD <braibaud@gmail.com>
2023-05-24 09:01:58 +0200

    init sorting

--- smaplib/fsio/__init__.py
+++ smaplib/fsio/__init__.py
@@ -2,7 +2,7 @@ from __future__ import absolute_import
 
 from . import array
 from . import common
+from . import dss
 from . import json
 from . import pickle
 from . import text
-from . import dss
--- smaplib/img/__init__.py
+++ smaplib/img/__init__.py
@@ -1,5 +1,6 @@
 from __future__ import absolute_import
 
-from . import helpers
 from . import classes
 from . import dss
+from . import helpers
+from . import plot
