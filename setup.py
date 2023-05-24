75d4135387dee522f8b0a235f3f71fb4a2145f7d
Benjamin RAIBAUD <braibaud@gmail.com>
2023-05-24 21:09:02 +0100

    deployment

--- setup.py
+++ setup.py
@@ -25,15 +25,15 @@ setup(
         'Topic :: Software Development :: Libraries :: Python Modules',
         'Topic :: Scientific/Engineering :: Artificial Intelligence'
     ],
-    install_requires=[
-        'annoy>=1.17.1,<2',
-        'dataiku-internal-client>=11.0.0,<12',
-        'pinecone-client>=2.2.1,<3',
-        'tensorflow-hub>=0.12.0,<1',
-        'tensorflow>=2.11.1,<3',
-        'matplotlib>=3.7.1,<4',
-        'pillow>=9.4.0,<10',
-        'pandas>=1.5.3,<2'
-    ],
+    # install_requires=[
+    #     'annoy>=1.17.1,<2',
+    #     'dataiku-internal-client>=11.0.0,<12',
+    #     'pinecone-client>=2.2.1,<3',
+    #     'tensorflow-hub>=0.12.0,<1',
+    #     'tensorflow>=2.11.1,<3',
+    #     'matplotlib>=3.7.1,<4',
+    #     'pillow>=9.4.0,<10',
+    #     'pandas>=1.5.3,<2'
+    # ],
     python_requires='>=3.8',
     packages=find_packages())
